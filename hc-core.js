a9d332c0b1066139f0e1ae43410ee7e23c57fa04
AzusaOu <happy_panda@126.com>
2019-05-15 02:42:36 +0900

    Please forget the previous commit...

--- hc-core.js
+++ hc-core.js
@@ -108,15 +108,85 @@ Auth: ${plain.a}`);
 	});
 
 	this.send_plaintxt = function (plaintxt, opposite_pbk) {
-		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(ch_share, plaintxt), self_pvk, opposite_pbk, 'utf-8');
-		client.publish(self_id_hash, msg);
+		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(self_id, plaintxt), self_pvk, opposite_pbk, 'utf-8');
+		client.publish(ch_share, msg);
 		console.log(`Send ${msg.length} bytes.`);
 	}
 
 	this.send_plainbin = function (plainBin, opposite_pbk, ext) {
 		const hash = hcp.sha(plainBin, 'md5');
-		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(ch_share, plainBin, 'hex', hash+ext), self_pvk, opposite_pbk, 'utf-8');
-		client.publish(self_id_hash, msg);
+		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(self_id, plainBin, 'hex', hash+ext), self_pvk, opposite_pbk, 'utf-8');
+		client.publish(ch_share, msg);
+		console.log(`Send ${msg.length} bytes.`);
+	}
+	
+	client_msg.on('message', function (ch, msg) {
+		const sMsg = lz.decompressFromBase64(msg.toString());
+		// Think how to decide opposite_pbk
+		const plain = en2Plain(sMsg, self_pvk, ut.get_key(room_id, false));
+		if (plain === -1) {
+			// console.log('*** Unreadable message');
+			return -1;
+		}
+		const recv = plain.r;
+		console.log(`
+===========================
+NEW MESSAGE @ ${ch}
+---------------------------
+From: ${recv.sender}
+Time: ${recv.time}
+Type: ${recv.type}
+Auth: ${plain.a}`);
+// send_cmd = function (type, content, ch_target, self_id, self_pvk, opposite_pbk)
+		// Command
+		if (recv.type === 'cmd') {
+			console.log('CMD::' + recv.info);
+			console.log(recv.payload);
+			// Auth passed
+			if (plain.a === true) {
+				// Receive invitation
+				if (recv.info === 'invitation') {
+					ch_share = recv.payload;
+					client_msg.subscribe(ch_share);
+					send_cmd(client, 'echo', ch_share, self_id_hash, self_id, self_pvk, ut.get_key(recv.sender));
+				// Receive echo
+				} else if (recv.info === 'echo') {
+					client_msg.subscribe(ch_share);
+				}
+			// Auth failed
+			} else {
+				console.log('Ignore invalid command');
+			}
+			
+		// Plain text
+		} else if (recv.type === 'utf-8') {
+			console.log('Contant:\n' + recv.payload);
+		
+		// Binary
+		} else if (recv.type === 'hex') {
+			console.log('Info: ' + recv.info);
+			fs.writeFile('./fileRecv/' + recv.info, Buffer.from(recv.payload, 'hex'), function (err) {
+				if (err) {
+					throw err;
+				} else {
+					console.log(`File ${recv.info} is saved.`);
+				}
+			});
+		}
+		console.log('===========================');
+		return 0;
+	});
+
+	this.send_plaintxt = function (plaintxt, opposite_pbk) {
+		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(self_id, plaintxt), self_pvk, opposite_pbk, 'utf-8');
+		client.publish(ch_share, msg);
+		console.log(`Send ${msg.length} bytes.`);
+	}
+
+	this.send_plainbin = function (plainBin, opposite_pbk, ext) {
+		const hash = hcp.sha(plainBin, 'md5');
+		const msg = hcp.gen_encryptedMsg(hcp.plainDataPackage(self_id, plainBin, 'hex', hash+ext), self_pvk, opposite_pbk, 'utf-8');
+		client.publish(ch_share, msg);
 		console.log(`Send ${msg.length} bytes.`);
 	}
 }
